66e0bed3d6a696df4b0bc71a6acb79f6edb6c0a7
Pau A <Heiwabeats@gmail.com>
2026-03-01 09:27:09 +0100

    schema zod updated

--- src/validators/item.schema.ts
+++ src/validators/item.schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
-export const createItemSchema = z.object({
+export const itemSchema = z.object({
   name: z.string().min(1),
   category: z.enum(["videogame", "book", "comic", "tcg", "figure", "other"]),
-  estimatedPrice: z.number().nonnegative(),
-  quantity: z.number().int().positive().default(1)
-});
+  estimatedPrice: z.number().positive(),
+  quantity: z.number().int().positive(),
+}).strict();
